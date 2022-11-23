d135310d684968f1102df239ca5dd53a474846f4
sjtorres <sandrotorres.ti@gmail.com>
2022-11-23 16:38:16 -0300

    Script add

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
-
 // adicionar uma lista---------------------------------------------
 
+const input = document.querySelector('.input')
+const ulContainer = document.querySelector('.ulContainer')
+const lixeira = document.querySelector('.lixeira')
+
+input.addEventListener('submit', event => {
+  event.preventDefault()
+
+  const inputValue = event.target.add.value.trim()
+
+  if (inputValue.length) {
+    ulContainer.innerHTML += `
+    <li class="li">
+      <span class="linha-li">${inputValue}</span>
+      <i class="bx bx-trash lixeira"></i>
+    </li> 
+    `
+
+    event.target.reset()
+  }
+
+})
 
 // lixeira-----------------------------------------------
 
+ulContainer.addEventListener('click', event => {
+  const clickElement = event.target
 
+  if(Array.from(clickElement.classList).includes('lixeira')) {
+    clickElement.parentElement.remove()
+  }
+})
